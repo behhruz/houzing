24196fc679909626d6e7566e6ec3826c451e3eed
behhruz <bsatimbayev05@gmail.com>
2023-03-03 19:38:50 +0500

    navbar finished

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import NavbarUttils from "../../utilities/navbar";
-import { Container, Links, Logo, Section, Wrapper } from "./style";
+import { Button, Container, Links, Logo, Section, Wrapper } from "./style";
 
 export const Navbar = () => {
   const navigate = useNavigate();
@@ -12,11 +12,21 @@ export const Navbar = () => {
           <Logo /> <h3>Houzing </h3>
         </Section>
         <Section>
-          {NavbarUttils.map(({ title, path,id }) => {
-            return <Links key={id} to={path} className={({isActive})=>isActive && 'active'}>{title}</Links>;
+          {NavbarUttils.map(({ title, path }, index) => {
+            return (
+              <Links
+                key={index}
+                to={path}
+                className={({ isActive }) => isActive && "active"}
+              >
+                {title}
+              </Links>
+            );
           })}
         </Section>
-        <Section>1</Section>
+        <Section>
+          <Button>Login</Button>
+        </Section>
       </Wrapper>
       <Outlet />
     </Container>

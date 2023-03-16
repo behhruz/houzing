1a26573e6ebb9e19cac7e0c55ec552aa5380cd1e
behhruz <bsatimbayev05@gmail.com>
2023-03-16 09:51:30 +0500

    done

--- src/components/generic/index.jsx
+++ src/components/generic/index.jsx
@@ -5,7 +5,7 @@ export const Button = ({ children, type, onClick }) => {
   return (
     <>
       <GenericButton onClick={onClick} type={type}>
-        Login
+        Login out
       </GenericButton>
     </>
   );
